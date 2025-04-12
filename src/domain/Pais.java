b3a4b17186bd8226399b6c6bedf63aaa1362249c
Franco Reyes <francoreyes597@gmail.com>
2025-04-12 16:12:46 -0300

    Actualizacion de clase pais

--- src/domain/Pais.java
+++ src/domain/Pais.java
@@ -2,14 +2,22 @@ package domain;
 
 
 public class Pais {
-    private String nombre;
+private final String nombre;
+    private final String codigoIso;
 
-    public Pais(String nombre) {
+    public Pais(String nombre, String codigoIso) {
         this.nombre = nombre;
+        this.codigoIso = codigoIso;
+
     }
 
     public String getNombre() {
         return nombre;
+    }
+
+
+    public String getCodigoIso() {
+        return codigoIso;
     }
     
     
